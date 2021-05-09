44f6c01bca9e670ada5fe79235184c3c3e0e1d3f
Shauryan Singh <shauryan23singh@gmail.com>
2021-05-09 22:03:09 +0530

    Add Comments & Clearing Input Field For New Session

--- Quiz.py
+++ Quiz.py
@@ -1,19 +1,18 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
-import random
-import sqlite3
-import threading
+import random # For randomizing the sequence in which questions are asked and their options are displayed
+import sqlite3 # For maintaining the data of playername and their scores, adding questions & options in database
 
 game_score = 0
-questions_attempted = 1
 player_name = ""
-qualifying_score = 2
-errors = None
+questions_attempted = 1 # For keeping count of questions diplayed(it'll be used to diplay FINISH BUTTON on the final question)
+qualifying_score = 3
+errors = None # For Error Checking while uploading question & options in database
 
 #*********************** TIMER **************************#
 
-time_in_sec = 20
+time_in_sec = 20 # Total Time for Quiz
 
 def timer():
     global time_in_sec, mylabel, button, myoptionlabel
@@ -43,20 +42,24 @@ c = conn.cursor()
 #             )
 #             """)
 
+# This function will be used later to Check If the player already exists in database
 def get_player_name(player_name):
     c.execute("SELECT * FROM QuizData WHERE name=:name", {'name':player_name})
     return c.fetchone()
 
+# This function is used to add player to the database with score
 def insert_player(player_name, game_score):
     with conn:
         c.execute("INSERT INTO quizData VALUES (:name, :score)", {'name': player_name, 'score': game_score})
 
+# This function is used to update the score of the player if he exists in the database already
 def update_score(player_name, game_score):
     with conn:
         c.execute("""UPDATE QuizData SET score = :score
                     WHERE name = :name""",
                   {'name': player_name, 'score': game_score})
 
+# To Create Table for User Added Questions and Options
 # c.execute("""CREATE TABLE QuestionsData(
 #             questions text,
 #             options text,
@@ -64,10 +67,12 @@ def update_score(player_name, game_score):
 #             )
 #             """)
 
+# To Add Question & Options in Database
 def insert_question_options(question, options, correct_option):
     with conn:
         c.execute("INSERT INTO QuestionsData VALUES (:db_question, :db_options, :db_correct_option)", {'db_question':question, 'db_options':options, 'db_correct_option':correct_option})
 
+# To load Questions & Options from Database
 def load_questions_options():
     with conn:
         c.execute("SELECT * FROM QuestionsData")
@@ -76,38 +81,42 @@ def load_questions_options():
 #************** DATABASE DATABASE DATABASE **************#
 
 
-#********************* Load Questions Into Lists & Dictionary ************************#
-
+# Function To Raise The Chosen Frame
 def show_frame(frame):
     frame.tkraise()
-    
-def load_questions_options_curr_session():
-    global questions, solutions, answers
-    curr_session = load_questions_options()
-    for curr_data in curr_session:
-        curr_question = curr_data[0]
-        curr_options = eval(curr_data[1])
-        curr_correct_option = curr_data[2]
-        if(curr_question not in questions):
-            questions.append(curr_question)
-            answers.append(curr_options)
-            solutions[curr_question] = curr_correct_option
 
+# To Extract Player Details, Start Timer And Load Questions 
 def submit_form():
-    load_questions_options_curr_session()
     global player_name
+    load_questions_options_curr_session()
     player_name = input_name.get().split()[0]
     player_name = player_name.lower()
     frame3.tkraise()
     timer()
     load_questions()
 
+# To Extract Questions & Options From Database And append it to the Lists & Dictionary respectively
+def load_questions_options_curr_session():
+    global questions, solutions, answers
+    curr_session = load_questions_options()
+    for curr_data in curr_session:
+        curr_question = curr_data[0]
+        curr_options = eval(curr_data[1])       
+        curr_correct_option = curr_data[2]
+        
+        if(curr_question not in questions):
+            questions.append(curr_question)
+            answers.append(curr_options)
+            solutions[curr_question] = curr_correct_option
+    
+# Main Quiz Logic
 def load_questions():
-    global questions_attempted, game_score, mylabel, button, myoptionlabel, time_in_sec
-    button = Button(frame3, image=next_btn, cursor = "hand2", borderwidth=0, bg="black", command=lambda: var.set(1))
+    global game_score, mylabel, button, myoptionlabel, time_in_sec, questions_attempted
+    button = Button(frame3, image=next_btn, cursor = "hand2", borderwidth=0, bg="black", command=lambda: var.set(1)) # Next Button
     button.place(x=700, y= 600)
-    random.shuffle(questions)
-    for question in questions:
+    random.shuffle(questions) # Shuffling Questions Sequence
+    curr_sesh_questions = questions[0:4] # To load Only 4 Questions For Current Session
+    for question in curr_sesh_questions:
         mylabel = Label(frame3, text=question, anchor=CENTER, font =("times new roman", 18, "bold"), bg= "black", fg= "white", wraplength= 800)
         mylabel.place(x=485, y=225)
         for value,key in solutions.items():
@@ -121,28 +130,31 @@ def load_questions():
                         option4 = answer[3]
                         myoptionlabel = Label(frame3, bg="black")
                         myoptionlabel.place(x=700, y=330)
+                        # RadioButtons For 4 Options 
                         Radiobutton(myoptionlabel, text=option1, font =("times new roman", 18, "bold"), bg="black", fg="white", variable=var2, value=option1, selectcolor="#000000").pack(pady=5, anchor="w")
                         Radiobutton(myoptionlabel, text=option2, font =("times new roman", 18, "bold"), bg="black", fg="white", variable=var2, value=option2, selectcolor="#000000").pack(pady=5, anchor="w")
                         Radiobutton(myoptionlabel, text=option3, font =("times new roman", 18, "bold"), bg="black", fg="white", variable=var2, value=option3, selectcolor="#000000").pack(pady=5, anchor="w")
                         Radiobutton(myoptionlabel, text=option4, font =("times new roman", 18, "bold"), bg="black", fg="white", variable=var2, value=option4, selectcolor="#000000").pack(pady=5, anchor="w")
                         button.wait_variable(var)
                         selected_option = var2.get()
-                        if selected_option == key:
+                        if selected_option == key: # To Check If Right Option Is Selected
                             game_score += 1
                         questions_attempted += 1
-                        if questions_attempted == 4:
-                            button.destroy()
+                        if questions_attempted == 4: # To Check If it's the Last Question
+                            # Destroy Previous Next Button And Create a New Button with Finish Text
+                            button.destroy() 
                             button = Button(frame3, image=finish_btn, cursor = "hand2", borderwidth=0, bg="black", command=lambda: var.set(1))
                             button.place(x=700, y= 600)
                         mylabel.destroy()
                         myoptionlabel.destroy()
-    button.destroy()
-    time_in_sec = 0
+    #button.destroy()
+    time_in_sec = 0 # To Stop The Timer and Resume The Application Execution Process From Else Part Of The Timer
 
 def completed_session():
-    data_handling()
+    data_handling() # To Push All Data To The Database
     messagebox.showinfo("    Quiz Game", "Quiz Has Finished\n{} you have scored {} out of 4".format(player_name, game_score))
     global qualify_bg, qualify_ok_btn, qualify_mssg, qualify_fail_mssg
+    # To Check If Player Has Passed or Failed The Quiz
     if(game_score >= qualifying_score):
         top = Toplevel()
         top.geometry("800x450")
@@ -165,13 +177,16 @@ def completed_session():
 
 def data_handling():
     global player_name
+    # To Check If Player Already Exists in Database If Not Then Insert The Player 
     if(get_player_name(player_name) == None):
         insert_player(player_name, game_score)
+    # If Player Already Exists Then Update His New Score If it is Greater Than The Previously In Database
     else:
         fetched_player = get_player_name(player_name)
         if(fetched_player[1] < game_score):
             update_score(player_name, game_score)
 
+# To Load Top 3 Players From Database
 def load_highscores(frame):
     frame.tkraise()
     c.execute("""SELECT * FROM 'QuizData'
@@ -183,7 +198,7 @@ def load_highscores(frame):
     txt_label = Label(frame5, font =("Courier New", 20,"bold"), text=const_txt, pady=25, bg="#f8f8f8")
     txt_label.place(x=475, y=75)
     
-    disty = 225
+    disty = 225 # This Variable Increments Accordingly To Place The Name & Score Label
 
     for x in range(0, 3):
         curr_player = highscores[x][0]
@@ -197,14 +212,20 @@ def load_highscores(frame):
 
         disty += 90
 
+# To Reinitialize All The Values & Jump To HomePage
 def refresh_session():
-    global player_name, game_score, time_in_sec
-    player_name = ""
+    global player_name, game_score, time_in_sec, input_name
+    questions_attempted = 1
+    #player_name = ""
+    input_name.delete(0, 'end')
+    txt_mail.delete(0, 'end')
+    txt_contact.delete(0, 'end')
     var2.set(' ')
     game_score = 0
     time_in_sec = 20
     show_frame(frame1)
 
+# To Add Questions & Options In The Database
 def add_questions():
     global input_question, input_options, input_correct_option, my_canvas, submit_btn
     top3 = Toplevel()
@@ -216,57 +237,65 @@ def add_questions():
     input_question = Entry(top3)
     my_canvas.create_window(150, 100, window=input_question, width=750, anchor="nw")
     input_question.config(font= ("Helvetica", 14),bg="#8319d3", fg="#ffffff", highlightthickness=5, highlightbackground="#74159d", highlightcolor="#74159d")
-    #Options
+    # Options
     my_canvas.create_text(153, 180, text= "Enter Options :", font= ("Helvetica", 18,"bold"), fill="#ffffff", anchor="nw")
     input_options = Entry(top3)
     input_options.config( font= ("Helvetica", 14),bg="#8319d3", fg="#ffffff", highlightthickness=5, highlightbackground="#74159d", highlightcolor="#74159d")
     my_canvas.create_window(150, 210, window=input_options, width=750, anchor="nw")
-    #Guide
+    # Guide
     my_canvas.create_text(153, 255, text= "**Enter 4 Options Above", font= ("Helvetica", 10,"bold"), fill="#e31313", anchor="nw")
     my_canvas.create_text(153, 273, text= "**Enter Comma Seperated values", font= ("Helvetica", 10,"bold"), fill="#e31313", anchor="nw")
-    #Correct Answers
+    # Correct Answers
     my_canvas.create_text(153, 330, text= "Correct Option :", font= ("Helvetica", 18,"bold"), fill="#ffffff", anchor="nw")
     input_correct_option = Entry(top3)
     input_correct_option.config( font= ("Helvetica", 14),bg="#8319d3", fg="#ffffff", highlightthickness=5, highlightbackground="#74159d", highlightcolor="#74159d")
     my_canvas.create_window(153, 360, window=input_correct_option,width=750, anchor="nw")
     my_canvas.create_text(153, 405, text= "*ENTER THE CORRECT ANSWER ABOVE(Note: It Must Be Present In The Enter Options Row **CASE SENSITIVE ", font= ("Helvetica", 10,"bold"), fill="#e31313", anchor="nw")
-    #Submit Button
+    # Submit Button
     submit_btn = Button(my_canvas, image=submit_btn_img, border=0, relief=FLAT, bg="#5f4bd1", cursor="hand2", command=handle_questions_options)
     submit_btn_window = my_canvas.create_window(450, 540, anchor="nw", window=submit_btn)
-    #Exit Button
+    # Exit Button
     form_exit_button = Button(my_canvas, image=form_exit_btn, cursor = "hand2", borderwidth=0, bg="#683ed2", command=top3.destroy)
     exit_btn_window = my_canvas.create_window(1025, 10, anchor="nw", window=form_exit_button)
 
+# To Extract Questions & Options From Entrybox
 def handle_questions_options():
-    global get_question, get_options, get_correct_option, my_canvas, submit_btn, err_var#, questions, answers, solutions
+    global get_question, get_options, get_correct_option, my_canvas, submit_btn, err_var
     get_question = input_question.get()
     get_options = input_options.get().split(",")
     get_correct_option = input_correct_option.get()
+    # To Remove Whitespaces if any from options
     for option in get_options:
         formatted_option = option.strip()
         formatted_options_list.append(formatted_option)
     get_correct_option = get_correct_option.strip()
 
+    # Error Checking Function Call
     validate_questions_options_data()
 
-    # temp_error_evaluator = errors
+# Function Which Runs In Loop For Error Checking
 def loop_check():
     global submit_btn, errors, err_var, get_options, get_correct_option, get_question
     while(errors):
         submit_btn.wait_variable(err_var)
         handle_questions_options()
     
-    #CONTINUE FROM HERE - TO FORWARD THE DATA TO RESPECTIVE LISTS AND DICTIONARY AND CLEAR THE PREVIOUS FETCHED DATA
+    # Converting List Consisting Of Options Into String To Push it Into The Database
     get_options = repr(get_options)
+    
+    # Adding Data To Database
     insert_question_options(get_question, get_options, get_correct_option)
     
+    # Question Added Successfully Message
     success_mssg_label = Label(my_canvas, text="Question Added Successfully!!!", bg="#5f4bd1", fg="#08ff08", font= ("Helvetica", 30,"bold"), anchor="nw")
     success_mssg_label.place(x=250, y=460)
     success_mssg_label.after(5000, success_mssg_label.destroy)
 
+# Function To Check For Errors If Present
 def validate_questions_options_data():
     global get_options, get_correct_option, err_var, errors
 
+    # Function Which Sets Error Value To True & Enters The Loop
     def error_validator():
         global errors
         err_var.set(1)
@@ -274,6 +303,7 @@ def validate_questions_options_data():
         top2.destroy()
         loop_check()
 
+    # TO Check if Only 4 Options are Present 
     if(len(get_options) != 4):
         top2 = Toplevel()
         top2.geometry("750x325")
@@ -284,6 +314,7 @@ def validate_questions_options_data():
         ok_button = Button(error_canvas, image=ok_btn, cursor = "hand2", borderwidth=0, command=error_validator)
         ok_btn_window = error_canvas.create_window(315, 220, anchor="nw", window=ok_button)
     
+    # To Check if Correct Option is Present in Both Rows
     elif(get_correct_option not in formatted_options_list):
         top2 = Toplevel()
         top2.geometry("925x325")
@@ -293,7 +324,8 @@ def validate_questions_options_data():
         error_canvas.create_text(120, 70, text= "Please Check If Correct Option Is Present In Both The Input Rows", font= ("Helvetica", 18,"bold"), fill="#e31313", anchor="nw")
         ok_button = Button(error_canvas, image=ok_btn, cursor = "hand2", borderwidth=0, command=error_validator)
         ok_btn_window = error_canvas.create_window(375, 220, anchor="nw", window=ok_button)
-
+    
+    # Setting Errors Value to False And Entering The Loop
     else:
         errors = False
         loop_check()
@@ -413,21 +445,18 @@ var = IntVar()
 var2 = StringVar()
 var2.set(' ')
 
-
 questions = ["The ratio of width of our National flag to its length is",
              "The words 'Satyameva Jayate' inscribed below the base plate of the emblem of India are taken from",
              "'Kathakali' is a folk dance prevalent in which state",
              "The last Mahakumbh of the 20th century was held at"]
-            
+
 solutions = {"The ratio of width of our National flag to its length is": "2:3",
              "The words 'Satyameva Jayate' inscribed below the base plate of the emblem of India are taken from": "Mundak Upanishad",
              "'Kathakali' is a folk dance prevalent in which state": "Karnataka",
              "The last Mahakumbh of the 20th century was held at": "Haridwar"}
-            
+
 answers = [["3:5", "2:3", "2:4", "3:4"],["Rigveda", "Satpath Brahmana", "Mundak Upanishad", "Ramayana"],
             ["Karnataka", "Orissa", "Kerala", "Manipur"],["Nasik", "Ujjain", "Allahabad", "Haridwar"]]
-
-random.shuffle(questions)
 
 #============================================== FRAME 4 ==================================================#
 
@@ -480,8 +509,6 @@ input_correct_option = Entry()
 formatted_options_list = []
 
 err_var = IntVar()
-
-t1 = threading.Thread(target=timer)
 
 show_frame(frame1)
 
